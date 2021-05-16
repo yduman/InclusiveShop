4045ec6badad5fc8086cf42899ade16371f7febf
Yadullah Duman <yadullah.duman@maibornwolff.de>
2021-05-16 23:06:11 +0200

    Add skeleton for login screen

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+import 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { AppRegistry, Platform } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+if (Platform.OS === 'ios') {
+  MaterialCommunityIcons.loadFont();
+}
 
 AppRegistry.registerComponent(appName, () => App);
