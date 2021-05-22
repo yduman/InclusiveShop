ad44c27a4f809584127221b7a1eb21246376ffa0
Yadullah Duman <yadullah.duman@maibornwolff.de>
2021-05-22 20:01:48 +0200

    Refactor ProductCardProps

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { ImageSourcePropType } from "react-native";
+import { TouchableWithoutFeedback, ImageSourcePropType } from "react-native";
 
 import { ProductImage, ProductView, ProductLikeButton } from "./Styled";
 import ProductDescription from "./ProductDescription";
 import ProductSaleBadge from "./ProductSaleBadge";
 
-interface NewProductCardProps {
+interface ProductCardProps {
   img: ImageSourcePropType;
   brand: string;
   title: string;
@@ -15,25 +15,28 @@ interface NewProductCardProps {
   salePercent?: string;
 }
 
-export default function ProductCard(props: NewProductCardProps) {
+export default function ProductCard(props: ProductCardProps) {
   const { img, brand, title, type, price, salePrice, salePercent } = props;
 
   return (
-    <ProductView>
-      <ProductLikeButton
-        icon="heart-outline"
-        size={20}
-        onPress={() => console.log("me likey")}
-      />
-      <ProductImage source={img} resizeMode="cover" />
-      <ProductSaleBadge salePrice={salePrice} salePercent={salePercent} />
-      <ProductDescription
-        brand={brand}
-        title={title}
-        type={type}
-        price={price}
-        salePrice={salePrice}
-      />
-    </ProductView>
+    <TouchableWithoutFeedback
+      onPress={() => console.log("pressed product card")}>
+      <ProductView>
+        <ProductLikeButton
+          icon="heart-outline"
+          size={20}
+          onPress={() => console.log("me likey")}
+        />
+        <ProductImage source={img} resizeMode="cover" />
+        <ProductSaleBadge salePrice={salePrice} salePercent={salePercent} />
+        <ProductDescription
+          brand={brand}
+          title={title}
+          type={type}
+          price={price}
+          salePrice={salePrice}
+        />
+      </ProductView>
+    </TouchableWithoutFeedback>
   );
 }
